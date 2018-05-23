5880c3bfe05c944ecdc6df90003299227965a533
Sessi <sessi.boton@gmail.com>
2018-05-23 17:07:57 +0200

    Connexion and base

--- jams-client/src/app/app.module.ts
+++ jams-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule
