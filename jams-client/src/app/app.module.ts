ef5e7c1088610b49f48b137469b3b3f9f104d204
SBAYTTI Jihane <sbaytti.jihane@gmail.com>
2018-05-24 01:08:12 +0200

    Merging conflicting versions

--- jams-client/src/app/app.module.ts
+++ jams-client/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { EnseignantComponent } from './enseignant/enseignant.component';
-import { CreationqcmComponent } from './creationqcm/creationqcm.component';
+import { CreationqcmComponent } from './enseignant/creationqcm/creationqcm.component';
+import { NavbarComponent } from './enseignant/navbar/navbar.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { MesQcmComponent } from './enseignant/mesqcm/mes-qcm.component';
+import { AcceuilEnseignantComponent } from './enseignant/acceuilenseignant/acceuil-enseignant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
     EnseignantComponent,
-    CreationqcmComponent
+    CreationqcmComponent,
+    NavbarComponent,
+    EtudiantComponent,
+    MesQcmComponent,
+    AcceuilEnseignantComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/acceuil', pathMatch: 'full' },
+      {path: 'acceuil', component : AcceuilEnseignantComponent},
+      {path: 'mesqcm', component : MesQcmComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
