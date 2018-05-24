b2cd9d96fcce688d8e7237b063226345eccd5ade
marion1911 <marion.1911@hotmail.fr>
2018-05-24 15:07:43 +0200

    Correction Acceuil -> Accueil + Lien sur Connexion et sur Création QCM

---
+++ jams-client/src/app/enseignant/accueilenseignant/accueil-enseignant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuilenseignant',
+  templateUrl: './accueil-enseignant.component.html',
+  styleUrls: ['./accueil-enseignant.component.css']
+})
+export class AccueilEnseignantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
