5182d17561907d6acf719b9e17e08de46f56c1e7
Antoine Pichard <antoine.pichard28@hotmail.fr>
2018-05-24 19:46:11 +0200

    fdsgviuchg

--- jams-server/routes/professeur.js
+++ jams-server/routes/professeur.js
@@ -6,29 +6,62 @@ router.post('/create', function(req, res) {
     models.professeur.create({
         adresse: req.body.adresse,
         mdp: req.body.mdp
-    }).then(function() {
-        res.redirect('/');
+    }).then(function(d) {
+        res.send({status :1});
+    });
+});
+
+router.get('/verif', function(req, res) {
+    models.professeur.findAll({
+        attributes : ['id_professeur']
+        where:{
+            adresse:req.params.adresse
+            mdp: req.params.mdp
+        },
+    }).then(function(res) {
+
+        if(res.length == 0){
+            //callback({"status":0, "error":"NOT_FOUND","professeur":null});
+            res.send({status :0});
+        } else{
+            res.setHeader('Content-Type','application/json');
+            res.send(data.);
+            //var professeur = {"id_professeur":res[0].get("id_professeur"), "username":res[0].get("adresse")}
+            //callback({"status":1, "error":null, "professeur":professeur});
+        }
+
     });
 });
 
 router.get('/all', function(req, res) {
     models.professeur.findAll({
-        attributes : ['id_professeur']
+        adresse:req.params.adresse,
+        mdp: req.params.mdp
     }).then(function(data) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).send({status: "success", data: data});
+        res.setHeader('Content-Type','application/json');
+        res.send(data);
     });
 });
 
-router.get('/:user_id/destroy', function(req, res) {
+router.get('/:id_professeur/destroy', function(req, res) {
     models.professeur.destroy({
         where: {
-            id_professeur: req.params.user_id
+            id_professeur: req.params.id_professeur
         }
     }).then(function() {
-        res.redirect('/');
+        res.send({status :1});
     });
 });
 
+router.get('/:adresse', function(req, res) {
+    models.professeur.findAll({
+        where: {
+            adresse: req.body.adresse
+        }
+    }).then(function(data) {
+        res.setHeader('Content-Type','application/json');
+        res.send(data.valueOf(adresse));
+    });
+});
 
 module.exports = router;
