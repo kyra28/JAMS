66267e785bc573767214603dd25cd7d7bd96f224
marion1911 <marion.1911@hotmail.fr>
2018-05-24 13:33:16 +0200

    Création etudiantqcm

--- jams-client/src/app/app.module.ts
+++ jams-client/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { EnseignantComponent } from './enseignant/enseignant.component';
-import { CreationqcmComponent } from './creationqcm/creationqcm.component';
+import { CreationqcmComponent } from './enseignant/creationqcm/creationqcm.component';
+import { EtudiantqcmComponent } from './etudiant/etudiantqcm/etudiantqcm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
     EnseignantComponent,
-    CreationqcmComponent
+    CreationqcmComponent,
+    EtudiantqcmComponent
   ],
   imports: [
     BrowserModule
