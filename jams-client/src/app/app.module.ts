fe46334249c1da3c37a10486e5bf97b656b7f5d9
Sessi <sessi.boton@gmail.com>
2018-05-24 11:19:58 +0200

    Revert 3 commit ef5e7c1 efa49ce d59a4da

--- jams-client/src/app/app.module.ts
+++ jams-client/src/app/app.module.ts
@@ -1,34 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { EnseignantComponent } from './enseignant/enseignant.component';
-import { CreationqcmComponent } from './enseignant/creationqcm/creationqcm.component';
-import { NavbarComponent } from './enseignant/navbar/navbar.component';
-import { EtudiantComponent } from './etudiant/etudiant.component';
-import { MesQcmComponent } from './enseignant/mesqcm/mes-qcm.component';
-import { AcceuilEnseignantComponent } from './enseignant/acceuilenseignant/acceuil-enseignant.component';
+import { CreationqcmComponent } from './creationqcm/creationqcm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
     EnseignantComponent,
-    CreationqcmComponent,
-    NavbarComponent,
-    EtudiantComponent,
-    MesQcmComponent,
-    AcceuilEnseignantComponent,
+    CreationqcmComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      {path: '', redirectTo: '/acceuil', pathMatch: 'full' },
-      {path: 'acceuil', component : AcceuilEnseignantComponent},
-      {path: 'mesqcm', component : MesQcmComponent}
-    ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
