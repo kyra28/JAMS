43aaea22909f005bfef142c6b0c15117ab3afaec
Antoine Pichard <antoine.pichard28@hotmail.fr>
2018-05-25 16:55:35 +0200

    last

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JAMS
+
+
+Client :
+JAMS/jams-client
+Il faut installer node js et angular puis executer les commandes dans le dossier : 
+npm install
+ng serve
+serveur lancé sur : http://localhost:4200/
+
+Server :
+JAMS/jams-server
+npm install
+node app.js http://localhost:5353
+
+Socket :
+JAMS/jams-server/socket
+node serveur.js
+serveur lancé sur : 
+ - eleve : http://localhost:3000/
+ - prof : http://localhost:3000/prof
--- jams-server/app.js
+++ jams-server/app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var http = require('http');
 
 var routes = require('./routes/index');
-var users  = require('./routes/users');
 var qcm  = require('./routes/qcm');
 var classe  = require('./routes/classe');
 var professeur  = require('./routes/professeur');
@@ -19,13 +18,6 @@ var reponse  = require('./routes/reponse');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
-//app.set('view engine', 'html');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
 app.use('/qcm', qcm);
 app.use('/question', question);
 app.use('/classe', classe);
@@ -46,29 +37,6 @@ app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
 });
-
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-// no stacktraces leaked to user unless in development environment
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: (app.get('env') === 'development') ? err : {}
-    });
-});*/
-
-/*
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));*/
 
 module.exports = app;
 
